f7e381eb2278e06d4a7e70287f5a13fc501692de
John <john.19col@gmail.com>
2019-12-24 11:35:24 -0500

    Add GraphQL express connector to application

--- users-app/src/server.js
+++ users-app/src/server.js
@@ -1,8 +1,10 @@
-const express = require("express");
+const express = require('express');
+const expressGraphQL = require('express-graphql');
 
 const application = express();
+application.use('/graphql', expressGraphQL({ graphiql: true }));
 
 const port = 4000;
 application.listen(port, () => {
-  console.log(`Application up and running on port ${port}...`);
+  console.log(`Application up and running on http://localhost:${port}/`);
 });
